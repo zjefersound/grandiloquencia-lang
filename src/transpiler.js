f226c020dc9cefdf4a0b8ea89f107153fca9ea8d
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2025-06-26 16:44:47 +0000

    Update transpiler to remove "FIM DO BLOCO"

--- src/transpiler.js
+++ src/transpiler.js
@@ -1,40 +1,48 @@
-// Transpilador: Grandiloquência++ → JavaScript
 function grandiloquenciaParaJS(codigoGrandiloquente) {
-    // Substituições de sintaxe rebuscada para JS padrão
-    let jsCode = codigoGrandiloquente
-        // Declaração de variável
-        .replace(/DEFINIR DE MANEIRA EXPLÍCITA E IRREVOGÁVEL A VARIÁVEL DE NOME \[(.*?)\] COM O VALOR \[(.*?)\]/g, 'let $1 = $2;')
-        
-        // Condicional (if)
-        .replace(/NO CASO EM QUE A CONDIÇÃO \[(.*?)\] SE MOSTRAR VERDADEIRA, EXECUTAR O SEGUINTE BLOCO:/g, 'if ($1) {')
-        
-        // Else if
-        .replace(/CASO CONTRÁRIO, SE A CONDIÇÃO \[(.*?)\] FOR VERDADEIRA, PROCEDER COM:/g, '} else if ($1) {')
-        
-        // Else
-        .replace(/CASO CONTRÁRIO, PROCEDER COM:/g, '} else {')
-        
-        // Loop for
-        .replace(/PARA CADA ITERAÇÃO NUMERADA DE \[(.*?)\] ATÉ \[(.*?)\], INCREMENTANDO DE \[(.*?)\] EM \[(.*?)\], PROCEDER COM:/g, 'for (let i = $1; i <= $2; i += $4) {')
-        
-        // Função
-        .replace(/UMA SUBROTINA DENOMINADA \[(.*?)\] QUE, AO RECEBER \[(.*?)\] E \[(.*?)\], RETORNA A SUA ADIÇÃO/g, 'function $1($2, $3) { return $2 + $3; }')
-        
-        // Try/Catch
-        .replace(/TENTATIVA DE EXECUÇÃO DO BLOCO\. EM CASO DE MALOGRO, REALIZAR A CONDUTA DE CONTINGÊNCIA/g, 'try {')
-        .replace(/FIM DA TENTATIVA/g, '} catch (e) {')
-        
-        // Console.log
-        .replace(/IMPRIMIR NA SAÍDA PADRÃO O SEGUINTE TEXTO: \[(.*?)\]/g, 'console.log($1);')
-        
-        // Return
-        .replace(/DEVOLVER AO CHAMADOR O SEGUINTE VALOR: \[(.*?)\]/g, 'return $1;')
-        
-        // Fechamento de blocos
-        .replace(/FIM DO BLOCO/g, '}');
+    let linhasOriginais = codigoGrandiloquente
+        .replace(/DEFINIR DE MANEIRA EXPLÍCITA E IRREVOGÁVEL A VARIÁVEL DE NOME\s+\[(.*?)\]\s+COM O VALOR\s+\[(.*?)\]/g, 'let $1 = $2;')
+        .replace(/NO CASO EM QUE A CONDIÇÃO\s+\[(.*?)\]\s+SE MOSTRAR VERDADEIRA, EXECUTAR O SEGUINTE BLOCO:/g, 'if ($1):')
+        .replace(/CASO CONTRÁRIO, SE A CONDIÇÃO\s+\[(.*?)\]\s+FOR VERDADEIRA, PROCEDER COM:/g, 'else if ($1):')
+        .replace(/CASO CONTRÁRIO, PROCEDER COM:/g, 'else:')
+        .replace(/IMPRIMIR NA SAÍDA PADRÃO O SEGUINTE TEXTO:\s+\[(.*?)\]/g, 'console.log($1);')
+        .split('\n');
 
-    return jsCode;
+    const transpiled = [];
+    const indentStack = [0];
+
+    for (let i = 0; i < linhasOriginais.length; i++) {
+        const linha = linhasOriginais[i];
+        const indent = linha.match(/^\s*/)[0].length;
+        const trimmed = linha.trim();
+
+        if (!trimmed) continue;
+
+        // Fecha blocos se a indentação diminuiu
+        while (indent < indentStack[indentStack.length - 1]) {
+            indentStack.pop();
+            transpiled.push('    '.repeat(indentStack.length - 1) + '}');
+        }
+
+        if (trimmed.endsWith(':')) {
+            // Remove ":" e abre bloco
+            const declaracao = trimmed.slice(0, -1);
+            transpiled.push('    '.repeat(indentStack.length - 1) + declaracao + ' {');
+            indentStack.push(indent + 1);
+        } else {
+            // Linha comum
+            transpiled.push('    '.repeat(indentStack.length - 1) + trimmed);
+        }
+    }
+
+    // Fecha blocos restantes
+    while (indentStack.length > 1) {
+        indentStack.pop();
+        transpiled.push('    '.repeat(indentStack.length - 1) + '}');
+    }
+
+    return transpiled.join('\n');
 }
+
 
 class GrandiloquenciaTranspiler {
     transpile(source) {
